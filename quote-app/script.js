f3cf5cdf3a573c77faccf5905c96bb53c48171e3
Irina H <ischa15@web.de>
2024-01-11 12:56:16 +0100

    quote-app

---
+++ quote-app/script.js
@@ -0,0 +1,45 @@
+const button = document.querySelector("button");
+const quoteList = document.querySelector("main");
+
+// Load state from local storage
+let quotes = JSON.parse(localStorage.getItem("quotes")) || [];
+
+// Add a default quote to the app state
+if (quotes.length === 0) {
+  quotes.push({
+    quote:
+      "The question isn't who is going to let me; it's who is going to stop me.",
+    author: "Ayn Rand",
+  });
+}
+quoteList.append(quotes[0].quote, " - " + quotes[0].author);
+
+// Add button with eventhandler to get quotes by click
+button.addEventListener("click", () => {
+  getQuote();
+});
+
+// Function to get/fetch and render/display the quote from the API
+function getQuote() {
+  quoteList.innerHTML = "";
+
+  fetch("https://dummy-apis.netlify.app/api/quote")
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+    })
+    .then((data) => {
+      const quote = document.createElement("p");
+      quote.setAttribute("class", "quote");
+      quote.append(document.createTextNode(data.quote));
+
+      const author = document.createElement("p");
+      author.setAttribute("class", "author");
+      author.append(document.createTextNode("- " + data.author));
+
+      quoteList.append(quote, author);
+      // quoteList.append(document.createTextNode(data.quote));
+      // quoteList.append(document.createTextNode(data.author));
+    });
+}
